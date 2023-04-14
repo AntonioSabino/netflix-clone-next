50c06f6379e2b6a08069e3ffe0b1742b2d8d7e31
Antonio Sabino <aslimajr@outlook.com>
2023-04-14 00:43:52 -0300

    feat(MovieList): adicionar componente MovieList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import Billboard from '@/components/Billboard'
+import MovieList from '@/components/MovieList'
 import Navbar from '@/components/Navbar'
 import useCurrentUser from '@/hooks/useCurrentUser'
+import useMovieList from '@/hooks/useMovieList'
 import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
+  const { data: movies = [] } = useMovieList()
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trendin Now" data={movies} />
+      </div>
     </>
   )
 }
